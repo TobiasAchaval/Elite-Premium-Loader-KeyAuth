11cc6be5ab71556458107c51b6f1fea07b0b8a82
Skimming3764 <quadrant2390@outlook.com>
2025-04-30 00:04:54 +0400

    Form1.Designer.cs

---
+++ Elite-Loader/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace spacey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.regB = new Guna.UI2.WinForms.Guna2Button();
+            this.psdTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lgBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.userTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.erTxt = new System.Windows.Forms.Label();
+            this.guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2CheckBox1 = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // regB
+            // 
+            this.regB.Animated = true;
+            this.regB.AutoRoundedCorners = true;
+            this.regB.BackColor = System.Drawing.Color.Transparent;
+            this.regB.BorderRadius = 22;
+            this.regB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.regB.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.regB.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.regB.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.regB.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.regB.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.regB.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.regB.ForeColor = System.Drawing.Color.White;
+            this.regB.Location = new System.Drawing.Point(404, 287);
+            this.regB.Name = "regB";
+            this.regB.Size = new System.Drawing.Size(214, 46);
+            this.regB.TabIndex = 8;
+            this.regB.Text = "Register";
+            this.regB.UseTransparentBackground = true;
+            this.regB.Click += new System.EventHandler(this.regB_Click);
+            // 
+            // psdTxt
+            // 
+            this.psdTxt.Animated = true;
+            this.psdTxt.AutoRoundedCorners = true;
+            this.psdTxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(20)))), ((int)(((byte)(22)))));
+            this.psdTxt.BorderRadius = 21;
+            this.psdTxt.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.psdTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.psdTxt.CustomizableEdges.BottomRight = false;
+            this.psdTxt.CustomizableEdges.TopLeft = false;
+            this.psdTxt.CustomizableEdges.TopRight = false;
+            this.psdTxt.DefaultText = "";
+            this.psdTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.psdTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.psdTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.psdTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.psdTxt.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(20)))), ((int)(((byte)(22)))));
+            this.psdTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.psdTxt.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.psdTxt.HoverState.BorderColor = System.Drawing.Color.Silver;
+            this.psdTxt.Location = new System.Drawing.Point(184, 214);
+            this.psdTxt.Name = "psdTxt";
+            this.psdTxt.PasswordChar = '●';
+            this.psdTxt.PlaceholderText = "Password";
+            this.psdTxt.SelectedText = "";
+            this.psdTxt.Size = new System.Drawing.Size(433, 44);
+            this.psdTxt.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.psdTxt.TabIndex = 7;
+            this.psdTxt.UseSystemPasswordChar = true;
+            // 
+            // lgBtn
+            // 
+            this.lgBtn.Animated = true;
+            this.lgBtn.AutoRoundedCorners = true;
+            this.lgBtn.BackColor = System.Drawing.Color.Transparent;
+            this.lgBtn.BorderRadius = 22;
+            this.lgBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lgBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.lgBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.lgBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.lgBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.lgBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.lgBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lgBtn.ForeColor = System.Drawing.Color.White;
+            this.lgBtn.Location = new System.Drawing.Point(184, 287);
+            this.lgBtn.Name = "lgBtn";
+            this.lgBtn.Size = new System.Drawing.Size(214, 46);
+            this.lgBtn.TabIndex = 6;
+            this.lgBtn.Text = "Login";
+            this.lgBtn.UseTransparentBackground = true;
+            this.lgBtn.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // userTxt
+            // 
+            this.userTxt.Animated = true;
+            this.userTxt.AutoRoundedCorners = true;
+            this.userTxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(20)))), ((int)(((byte)(22)))));
+            this.userTxt.BorderRadius = 21;
+            this.userTxt.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.userTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.userTxt.CustomizableEdges.BottomRight = false;
+            this.userTxt.CustomizableEdges.TopLeft = false;
+            this.userTxt.CustomizableEdges.TopRight = false;
+            this.userTxt.DefaultText = "";
+            this.userTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.userTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.userTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.userTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.userTxt.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(20)))), ((int)(((byte)(22)))));
+            this.userTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.userTxt.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.userTxt.HoverState.BorderColor = System.Drawing.Color.Silver;
+            this.userTxt.Location = new System.Drawing.Point(184, 153);
+            this.userTxt.Name = "userTxt";
+            this.userTxt.PasswordChar = '\0';
+            this.userTxt.PlaceholderText = "Username";
+            this.userTxt.SelectedText = "";
+            this.userTxt.Size = new System.Drawing.Size(433, 44);
+            this.userTxt.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.userTxt.TabIndex = 5;
+            this.userTxt.TextChanged += new System.EventHandler(this.userTxt_TextChanged);
+            // 
+            // erTxt
+            // 
+            this.erTxt.AutoSize = true;
+            this.erTxt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.erTxt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.erTxt.ForeColor = System.Drawing.Color.IndianRed;
+            this.erTxt.Location = new System.Drawing.Point(12, 424);
+            this.erTxt.Name = "erTxt";
+            this.erTxt.Size = new System.Drawing.Size(152, 17);
+            this.erTxt.TabIndex = 11;
+            this.erTxt.Text = "Invalid Login Information";
+            // 
+            // guna2ShadowForm1
+            // 
+            this.guna2ShadowForm1.BorderRadius = 14;
+            this.guna2ShadowForm1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(148)))), ((int)(((byte)(215)))));
+            this.guna2ShadowForm1.TargetForm = this;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(283, 1);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(227, 152);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 22;
+            this.guna2PictureBox1.TabStop = false;
+            this.guna2PictureBox1.UseTransparentBackground = true;
+            // 
+            // guna2CheckBox1
+            // 
+            this.guna2CheckBox1.AutoSize = true;
+            this.guna2CheckBox1.Checked = true;
+            this.guna2CheckBox1.CheckedState.BorderColor = System.Drawing.Color.White;
+            this.guna2CheckBox1.CheckedState.BorderRadius = 0;
+            this.guna2CheckBox1.CheckedState.BorderThickness = 0;
+            this.guna2CheckBox1.CheckedState.FillColor = System.Drawing.Color.White;
+            this.guna2CheckBox1.CheckMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.guna2CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.guna2CheckBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2CheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.guna2CheckBox1.ForeColor = System.Drawing.Color.White;
+            this.guna2CheckBox1.Location = new System.Drawing.Point(186, 265);
+            this.guna2CheckBox1.Name = "guna2CheckBox1";
+            this.guna2CheckBox1.Size = new System.Drawing.Size(95, 17);
+            this.guna2CheckBox1.TabIndex = 23;
+            this.guna2CheckBox1.Text = "Remember Me";
+            this.guna2CheckBox1.UncheckedState.BorderColor = System.Drawing.Color.White;
+            this.guna2CheckBox1.UncheckedState.BorderRadius = 0;
+            this.guna2CheckBox1.UncheckedState.BorderThickness = 0;
+            this.guna2CheckBox1.UncheckedState.FillColor = System.Drawing.Color.White;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BorderRadius = 15;
+            this.guna2ControlBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(743, 12);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.guna2CheckBox1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.erTxt);
+            this.Controls.Add(this.regB);
+            this.Controls.Add(this.psdTxt);
+            this.Controls.Add(this.lgBtn);
+            this.Controls.Add(this.userTxt);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Elite Store | Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Button regB;
+        private Guna.UI2.WinForms.Guna2TextBox psdTxt;
+        private Guna.UI2.WinForms.Guna2Button lgBtn;
+        private Guna.UI2.WinForms.Guna2TextBox userTxt;
+        private System.Windows.Forms.Label erTxt;
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2CheckBox guna2CheckBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+    }
+}
+
