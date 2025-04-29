e2ff00ebe9e685d4f7a7e683f431c0f86101bdf4
Skimming3764 <quadrant2390@outlook.com>
2025-04-30 00:04:34 +0400

    chm.Designer.cs

---
+++ Elite-Loader/chm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace spacey
+{
+    partial class chm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // chm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "chm";
+            this.Size = new System.Drawing.Size(728, 421);
+            this.Load += new System.EventHandler(this.chm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
